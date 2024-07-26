652645b6506d175e8041b6d9685a9c1e45b23e12
Raghav Nayak <raghavendra.nayak@betterplace.co.in>
2024-07-26 22:39:13 +0530

    added another stage in the pipeline for prime number generator with random number

--- code/concurrency/pipelineWithGenerators/pipelineWithGenerators.go
+++ code/concurrency/pipelineWithGenerators/pipelineWithGenerators.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func repeatFunc[T any, K any](done <- chan K, fn func() T ) <- chan T {
@@ -38,16 +39,53 @@ func take[T any, K any](done <- chan K, stream <- chan T, num int) <- chan T {
 }
 
 
+func primeFinder(done <- chan int, randomIntStream <- chan int) <- chan int {
+
+	isPrime := func(randomInt int) bool {
+		for i:= 2; i < randomInt; i++ {
+			if randomInt % i == 0 {
+				return false
+			}
+		}
+		return true
+	}
+
+	primes := make(chan int)
+	go func() {
+		defer close(primes)
+		for {
+			select {
+				case <- done:
+					return
+				case randomInt := <- randomIntStream:
+					if isPrime(randomInt) {
+						primes <- randomInt
+					}
+			}
+		}
+	}()
+	return primes
+}
+
 
 func main() {
+	start := time.Now()
 	done := make(chan int)
 	defer close(done)
 
 	randomNumFetcher := func() int {
-		return rand.Intn(5000000)
+		return rand.Intn(10000000)
 	}
 
-	for randomNum := range take(done, repeatFunc(done, randomNumFetcher), 10){
+
+	randomIntStream := repeatFunc(done, randomNumFetcher)
+
+	primeStream := primeFinder(done, randomIntStream)
+
+	for randomNum := range take(done, primeStream, 10){
 		fmt.Println(randomNum)
 	}
+
+	fmt.Println(time.Since(start))
+
 }
