c524927b4993be77bc971d997b224506e875c696
Raghav Nayak <raghavendra.nayak@betterplace.co.in>
2024-07-30 18:37:42 +0530

    disadvantages of mutex

--- code/concurrency/simultaneousGoRoutinesWithMutex/simultaneousGoRoutinesWithMutex.go
+++ code/concurrency/simultaneousGoRoutinesWithMutex/simultaneousGoRoutinesWithMutex.go
@@ -3,20 +3,27 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 var lock sync.Mutex
 
+func process(data int) int {
+	time.Sleep(time.Second * 2)
+	return data * 2
+}
+
 func processData(wg *sync.WaitGroup, result *[]int, data int) {
 	lock.Lock()
 	defer wg.Done() // to inform the go routine that it is done
-	processedData := data * 2
+	processedData := process(data)
 	*result = append(*result, processedData)
 	lock.Unlock()
 }
 
 
 func main() {
+	start := time.Now()
 	var wg sync.WaitGroup
 
 	input := []int{1,2,3,4,5}
@@ -30,5 +37,7 @@ func main() {
 	}
 	wg.Wait()
 	fmt.Println(result)
+	fmt.Println(time.Since(start))
+
 }
 
